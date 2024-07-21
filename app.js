68ffcca4692bd0c4382544d235062b7f22d47c77
Gustavo0022 <gustavo.gomes.ggt@gmail.com>
2024-07-21 01:12:10 -0300

    arquivo CSS para pc criado

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 function criptografarTexto(){
-    let textoDescripto = document.querySelector('input');
+    let textoDescripto = document.querySelector('textarea');
     textoCripto = textoDescripto.value.replaceAll('e','enter').replaceAll('i','imes').replaceAll('a','ai').replaceAll('o','ober').replaceAll('u','ufat');
     let campoCripto = document.getElementById('saida');
     campoCripto.innerHTML = textoCripto;
 }
 function descriptografarTexto(){
-    let textoCripto = document.querySelector('input');
+    let textoCripto = document.querySelector('textarea');
     textoDescripto = textoCripto.value.replaceAll('ufat',' u').replaceAll('ober','o').replaceAll('ai','a').replaceAll('imes','i').replaceAll('enter','e');
     let campoDescripto = document.getElementById('saida');
     campoDescripto.innerHTML= textoDescripto;

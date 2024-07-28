40dbf7e55c2a4091b003d570c0a14658d2a266d8
Gustavo0022 <gustavo.gomes.ggt@gmail.com>
2024-07-27 23:20:11 -0300

    Adicionado Readme e melhorada visibilidade

---
+++ README.md
@@ -0,0 +1,30 @@
+
+# Decodificador de Texto (Alura)
+
+**Atenção:** Este projeto é o desafio final da formação Oracle ONE Next Education T7, em parceria com a Alura.
+
+
+## Objetivo
+
+Fazer um codificador e decodificador de texto com as tecnologias aprendidas no curso, por meio da substituição de caracteres por cadeias predefinidas de caracteres
+
+## Funções
+* **Codificação de decodificação de textos**
+* **Interface intuitiva e limpa**
+* **Responsividade em celulares e tablets**
+
+## Tecnologias
+ 
+Para o projeto, foram utilizados HTML, CSS e JavaScript, além do github para gerenciamento de versões e o Vercel para a publicação do projeto
+
+![Static Badge](https://img.shields.io/badge/HTML%20-%20logo?style=flat-square&logo=html5&logoColor=white&logoSize=auto&labelColor=%23E34F26&color=2b2b2d)
+![Static Badge](https://img.shields.io/badge/CSS%20-%20logo?style=flat-square&logo=CSS3&logoColor=white&logoSize=auto&labelColor=%231572B6&color=2b2b2d)
+![Static Badge](https://img.shields.io/badge/JavaScript%20-%20logo?style=flat-square&logo=JavaScript&logoColor=gray&logoSize=auto&labelColor=%23F7DF1E&color=2b2b2d)
+
+## Créditos
+
+Feito por Gustavo Gomes
+
+![Static Badge](https://img.shields.io/badge/Site%20pessoal%20-%20link?style=flat-square&labelColor=1e4b70&color=2b2b2d&link=https%3A%2F%2Fgustavo-gomes-home.vercel.app%2F)
+
+
--- assets/app.js
+++ assets/app.js
@@ -4,20 +4,20 @@ let placeholderSaida = document.querySelector('.body__pag__textovazio');
 let campoSaidaCheio = document.querySelector('.body__pag__com__texto');
 
 function criptografarTexto(){
-    mudarConteudodaSaida()
+    mudarConteudodaSaida();
     textoCripto = campoEntrada.value.replaceAll('e','enter').replaceAll('i','imes').replaceAll('a','ai').replaceAll('o','ober').replaceAll('u','ufat');
     campoSaida.innerHTML = textoCripto;
     
 }
 function descriptografarTexto(){
-    mudarConteudodaSaida()
+    mudarConteudodaSaida();
     textoDescripto = campoEntrada.value.replaceAll('ufat','u').replaceAll('ober','o').replaceAll('ai','a').replaceAll('imes','i').replaceAll('enter','e');
     campoSaida.innerHTML= textoDescripto;
 }
 function mudarConteudodaSaida(){
     if (campoEntrada.value == ""){
-        alert('Digite alguma coisa para criptografar ou descriptografar!!!')
-        exibirConteudo(placeholderSaida)
+        alert('Digite alguma coisa para criptografar ou descriptografar!!!');
+        exibirConteudo(placeholderSaida);
     }
     else if (campoEntrada.value != ""){
         ocultarConteudo(placeholderSaida);
@@ -25,11 +25,11 @@ function mudarConteudodaSaida(){
     }
 }
 function ocultarConteudo(elemento){
-    elemento.style.display = 'none'
+    elemento.style.display = 'none';
 }
 function exibirConteudo(elemento){
-    elemento.style.display = 'flex'
+    elemento.style.display = 'flex';
 }
 function copiarTexto(){
-    navigator.clipboard.writeText(campoSaida.value)
+    navigator.clipboard.writeText(campoSaida.value);
 }
